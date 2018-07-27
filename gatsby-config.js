d1f792ffb65e344a2fb2039a6096d6a281a60621
Stavan Himal <StavanHimal@Stavan-Himals-MacBook-Pro.local>
2018-07-27 21:15:03 +0200

    Watch The Video Text

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Design+Code React01',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to Code in React</h1>
+    <p>The course from Design+Code</p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
